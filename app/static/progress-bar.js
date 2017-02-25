ce64f429f06cdfd05b28aa7d05c524128ec5479f
Pete Hamilton <peterejhamilton@gmail.com>
2017-02-25 00:25:18 +0000

    Update progress bar
    - Experimented with a peloton, used coloured bikes but then switched to
    emoji
    - Use a more recognisable cyclist...

--- app/static/progress-bar.js
+++ app/static/progress-bar.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+
+const PelotonCyclist = React.createClass({
+  render: function() {
+    const {color, offset} = this.props;
+    return <div className="progress-bar-peloton-emoji" style={{right: 17 + offset}}>🚴</div>;
+    // return <div className="progress-bar-peloton" style={{backgroundColor: color, right: 17 + offset}}></div>;
+  }
+});
+
 // Fun stats relative to other random distances/durations
 export const ProgressBar = React.createClass({
   getInitialState: function () {
@@ -9,11 +18,26 @@ export const ProgressBar = React.createClass({
   },
 
   render: function() {
+    let peloton = [];
+
+    const numRiders = Math.round(Math.random() * 7) + 5
+    for(let i = 0; i < numRiders; i++) {
+      const offset = Math.round(Math.random()*200)
+      const colors = [
+        'red', 'green', 'marine', 'yellow', 'pink', 'purple',
+      ];
+      const color = colors[Math.round(Math.random()*colors.length)];
+      peloton.push(<PelotonCyclist key={i} color={color} offset={offset} />)
+    }
+
     return (
-      <div className="progress-bar">
-        <div className="progress-bar-inner" style={{ width: `${100 * this.state.percentage}%` }}>
-          <div className="progress-bar-inner-inner">
-            <img className="progress-bar-cyclist" src="/cyclist.svg" />
+      <div className="progress-bar-container">
+        <div className="progress-bar">
+          <div className="progress-bar-inner" style={{ width: `${100 * this.state.percentage}%` }}>
+            <div className="progress-bar-inner-inner">
+              <img className="progress-bar-grey" src="/grey.png" />
+              {peloton}
+            </div>
           </div>
         </div>
       </div>
