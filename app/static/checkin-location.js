b160339be92d9cde429fc2dd8fa17843332b883d
Pete Hamilton <peterejhamilton@gmail.com>
2017-02-25 07:49:47 +0000

    Fewer emoji

--- app/static/checkin-location.js
+++ app/static/checkin-location.js
@@ -27,7 +27,7 @@ export const CheckinLocation = React.createClass({
     return (
       <div className="stat">
         <div className="stat-heading">{this.state.locationDescription}</div>
-        <div className="stat-body">🎉 🎉 {location} 🎉 🎉</div>
+        <div className="stat-body">🎉 {location} 🎉</div>
         <div className="stat-footer">last checked in {timeAgo}</div>
       </div>
     );
