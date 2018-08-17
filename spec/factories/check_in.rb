8c7e2c70787c96218c5813f16e26fb8d4e81d6b4
Grey Baker <greysteil@gmail.com>
2018-08-17 08:26:37 +0100

    Upgrade factorybot

--- spec/factories/check_in.rb
+++ spec/factories/check_in.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :check_in do
-    battery_state "good"
-    message_type "OK"
-    latitude 61.54875
-    longitude -3.0697
-    spot_id "585079373"
-    sent_at { Time.now }
+    battery_state { "good" }
+    message_type  { "OK" }
+    latitude      { 61.54875 }
+    longitude     { -3.0697 }
+    spot_id       { "585079373" }
+    sent_at       { Time.now }
   end
 end
--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :photo do
-    latitude 61.54875
-    longitude -3.0697
-    url "https://example.com/image.jpeg"
-    taken_at { Time.now }
+    latitude               { 61.54875 }
+    longitude              { -3.0697 }
+    url                    { "https://example.com/image.jpeg" }
+    taken_at               { Time.now }
     sequence(:external_id) { |n| "external-id-#{n}" }
   end
 end
